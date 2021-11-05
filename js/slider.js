8ae9b80b339a02d354f0c5b3794846fe9366c5d1
Arslan Shahid <arslanshahid1@gmail.com>
2021-11-05 20:43:04 +0500

    done

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#lightSlider").lightSlider(
+    {
+      item: 3,
+      autoWidth: false,
+      slideMove: 1,
+      slideMargin: 10,
+      mode: "slide",
+      pager: false,
+      responsive : [
+        {
+            breakpoint:786,
+            settings: {
+                item:2,
+                slideMove:1,
+                slideMargin:6,
+              }
+        }, 
+        {
+            breakpoint:576,
+            settings: {
+                item:1,
+                slideMove:1
+              }
+        }
+    ]     
+    }
+  ); 
+});
